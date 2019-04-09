f0458bc1de0689a3657f33c1668d7984e55d56b4
btrb <thesupervisar@gmail.com>
2019-04-09 20:13:13 -0300

    wip

--- tutorial_usp/c_spaces.py
+++ tutorial_usp/c_spaces.py
@@ -1,15 +1,17 @@
 # Took from `devito/tests/conftest.py`
-from devito.tools import as_tuple
-def EVAL(exprs, *args):
-    scope = {}
-    for i in args:
-            scope[i.name] = i
-            for j in i.function.indices:
-                scope[j.name] = j
-    processed = []
-    for i in as_tuple(exprs):
-        processed.append(eval(i, globals(), scope))
-    return processed[0] if isinstance(exprs, str) else processed
+# from devito.tools import as_tuple
+# def EVAL(exprs, *args):
+#     scope = {}
+#     for i in args:
+#         scope[i.name] = i
+#         for j in i.function.indices:
+#             scope[j.name] = j
+#     processed = []
+#     for i in as_tuple(exprs):
+#         print(i)
+#         print(scope)
+#         processed.append(eval(i, globals(), scope))
+#     return processed[0] if isinstance(exprs, str) else processed
 
 
 ##
@@ -21,13 +23,9 @@ from devito.types import Array
 t0i = Array(name='t0i', shape=(3,5,7), dimensions=(x, y, z), scope='heap')
 t1i = Array(name='t1i', shape=(3,5,7), dimensions=(x, y, z), scope='heap')
 
-
-
-exprs = ['Eq(t0i[x,y,z], t1i[x,y,z])',
-         'Eq(t1i[x,y,z], t0i[x,y,z])']
-
 from devito import Eq
-eq1, eq2 =  EVAL(exprs, t0i, t1i)
+eq1 = Eq(t0i[x,y,z], t1i[x,y,z])
+eq2 = Eq(t1i[x,y,z], t0i[x,y,z])
 
 from devito.ir.equations import LoweredEq
 expr1 = LoweredEq(eq1)
@@ -35,4 +33,8 @@ expr2 = LoweredEq(eq2)
 
 # print(eq1)
 # print(expr1)
-# print(eq1 == expr1)
+# eq1 == expr1
+# eq1.dspace !
+# expr1.dspace
+# expr1.ispace
+# expr1.ispace.directions
