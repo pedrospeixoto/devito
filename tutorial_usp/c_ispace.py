0f192d166f23ff1d9ec5f78b46aa3d089f65eef6
btrb <thesupervisar@gmail.com>
2019-04-20 17:33:53 -0300

    adding d_dependence2

--- tutorial_usp/c_ispace.py
+++ tutorial_usp/c_ispace.py
@@ -1,31 +1,66 @@
-from devito.types import Scalar, Constant
-
-A = Scalar(name='A')
-B = Scalar(name='C')
-C = Scalar(name='C')
-D = Scalar(name='D')
-
-from devito import Eq
-
-eqs = [Eq(A,1),
-       Eq(B,A+2),
-       Eq(A,C-D),
-       Eq(A,B/C)]
-
+from devito import Grid, Eq
+from devito.types import Array
 from devito.ir.equations import LoweredEq
-exprs = [LoweredEq(i) for i in eqs]
+from devito.ir.support.basic import Scope, IterationInstance
 
-from devito.ir.support.basic import Scope
-scope = Scope(exprs)
-print(scope)
+grid = Grid((10))
+x, = grid.dimensions
 
-deps = scope.d_flow
-print(deps)
+a = Array(name='a', shape=grid.shape, dimensions=grid.dimensions, scope='heap')
+b = Array(name='b', shape=grid.shape, dimensions=grid.dimensions, scope='heap')
+c = Array(name='c', shape=grid.shape, dimensions=grid.dimensions, scope='heap')
+d = Array(name='d', shape=grid.shape, dimensions=grid.dimensions, scope='heap')
 
 
+exprs1 = [LoweredEq(Eq(a[x], b[x] + c[x])),
+        LoweredEq(Eq(d[x], a[x]))]
 
-# for i in exprs:
-#     print(i.ispace._directions)
+scope1 = Scope(exprs1)
+scope1
+
+dep1 = scope1.d_all[0]
+dep1.is_flow
+dep1.is_carried
+dep1.cause
+
+##
+
+exprs2 = [LoweredEq(Eq(a[x], b[x] + c[x])),
+        LoweredEq(Eq(d[x], a[x-1]))]
+
+scope2 = Scope(exprs2)
+scope2
+
+dep2 = scope2.d_all[0]
+dep2.is_flow
+getattr(dep2, 'is_carried')()
+dep2.cause
+
+exprs3 = [LoweredEq(Eq(a[x], b[x] + c[x])),
+          LoweredEq(Eq(d[x], a[x+10]))]
+
+scope3 = Scope(exprs3)
+
+len(getattr(scope1, 'd_flow'))
+len(getattr(scope1, 'd_anti'))
+len(getattr(scope2, 'd_flow'))
+len(getattr(scope2, 'd_anti'))
+len(getattr(scope3, 'd_flow'))
+len(getattr(scope3, 'd_anti'))
+
+##
+
+
+IterationInstance
+
+
+# scope1
+
+# exprs3 = [LoweredEq(Eq(a[x], b[x] + c[x])),
+#           LoweredEq(Eq(d[x], a[x-1]))]
+
+# ### 
+
 
 # from devito import Grid
 # grid = Grid((3,3,3))
@@ -45,10 +80,10 @@ print(deps)
 #        Eq(t0i[x,y,z], t0i[x+1,y+2,z]),
 #        Eq(t0i[x,y,z], t0i[x,y+2,z-3])]
 
-# # print(eqs[0].ispace) !
+# # # print(eqs[0].ispace) !
 
-# from devito.ir.equations import LoweredEq
-# exprs = [LoweredEq(i) for i in eqs]
+# # from devito.ir.equations import LoweredEq
+# # exprs = [LoweredEq(i) for i in eqs]
 
-# for i in exprs:
-#     print(i.ispace)
+# # for i in exprs:
+# #     print(i.ispace)
