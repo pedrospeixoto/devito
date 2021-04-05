e1ae3e90ccde3b88a8562d7f0011a827688c8fe8
Pedro Peixoto <ppeixoto@usp.br>
2021-04-05 13:10:44 -0300

    clea data

--- examples/seismic/cemeai/gerar_modelos.py
+++ examples/seismic/cemeai/gerar_modelos.py
@@ -1,77 +1,241 @@
-#NBVAL_IGNORE_OUTPUT
-from examples.seismic import Model, plot_velocity
+#==============================================================================
+# Bibliotecas Python
+#==============================================================================
 import numpy as np
 import time
+#==============================================================================
 
+#==============================================================================
+# Bibliotecas Devito
+#==============================================================================
+from examples.seismic import Model, plot_velocity
+#==============================================================================
 
-def generateRandomModel(shape, spacing, origin):
+#==============================================================================
+def generateRandomModel(teste,vmodelmin,vmodelmax,max_layers):
 
-    # Define a velocity profile. The velocity is in km/s
-    v = np.empty(shape, dtype=np.float32)
+    shape      = (teste.nptx,teste.nptz)
+    spacing    = (teste.hx,teste.hz)
+    origin     = (teste.x0,teste.z0)
+    sou        = teste.sou
+    nbl        = teste.nbl
+    v          = np.empty(shape, dtype=np.float32)
 
-    max_layers = 7;
-    incl = np.random.rand(7) * 0.2 - 0.1; # numeros aleatorios entre -0.1 e 0.1
-    ampl = np.random.rand(7) * 20;
-    wlen = np.random.rand(7) * 0.5;
-    cent = np.random.rand(7) * shape[0];
+    vel_media    = np.sort(np.random.rand(max_layers+1)*int(vmodelmax-vmodelmin) + vmodelmin)
+    vel_media[0] = vmodelmin;
 
-    vel_media = np.sort(np.random.rand(8) * 2 + 1.5)
-    vel_media[0] = 1.5; ######## restricao
+    layer_type    = np.rint(np.random.rand(max_layers)).astype(int)
+    layer_type[0] = 0;  
 
-    # sorteio da equacao para cada layer (sin ou reta)
-    layer_type = np.rint(np.random.rand(7)).astype(int)
-    layer_type[0] = 0;  ######## restricao
+    n_layers = np.floor(np.random.rand() * max_layers + 1).astype(int)
 
-    # sorteio do numero de camadas
-    n_layers = np.floor(np.random.rand() * 7 + 1).astype(int)
-
-
-    prof = np.random.rand(n_layers) * np.rint(shape[1]/n_layers);
-    prof[0] = np.rint(10) ######## restricao
+    prof    = np.random.rand(n_layers) * np.rint(shape[1]/n_layers);
+    prof[0] = np.rint(10)
 
     for k in range(n_layers):
+        
         prof[k] = prof[k] + np.rint(shape[1]/n_layers)*k;
-
     
-    k_vec = np.arange(0, n_layers)
-    i_vec = np.arange(0, shape[0])
-    j_vec = np.arange(0, shape[1])
+    k_vec = np.arange(0,n_layers)
+    i_vec = np.arange(0,shape[0])
+    j_vec = np.arange(0,shape[1])
 
-    eq_layer = np.zeros( (n_layers, shape[0]) )
+    eq_layer = np.zeros((n_layers, shape[0]))
     
-    for k in range(0, n_layers):
-        if layer_type[k] == 0:
-            eq_layer[k, :] = incl[k] * i_vec + prof[k];
+    incl = np.random.rand(max_layers) * 0.2 - 0.1; 
+    ampl = np.random.rand(max_layers) * 20;
+    wlen = np.random.rand(max_layers) * 0.5;
+    cent = np.random.rand(max_layers) * shape[0];
+    
+    for k in range(0,n_layers):
+        
+        if(layer_type[k]==0):
+        
+            eq_layer[k,:] = incl[k] * i_vec + prof[k];
+        
         else:
-            #eq_layer = ampl[k] * np.sin(wlen[k] * i) + prof[k];
-            eq_layer[k, :] = -ampl[k] * np.exp(-0.05 * wlen[k] * (i_vec-cent[k]) * (i_vec-cent[k])) + prof[k] + incl[k] * i_vec;
+        
+            eq_layer[k,:] = -ampl[k] * np.exp(-0.05 * wlen[k] * (i_vec-cent[k]) * (i_vec-cent[k])) + prof[k] + incl[k] * i_vec;
 
     i_vec = np.array([i_vec,]*shape[1]).transpose()
     j_vec = np.array([j_vec,]*shape[0])
 
     v[:, :] = vel_media[n_layers]
+    
     for k in range(n_layers-1, -1, -1):
+    
         indices = j_vec < eq_layer[k, i_vec]
+        
         v[np.where(indices)] = vel_media[k]
 
-    # With the velocity and model size defined, we can create the seismic model that
-    # encapsulates this properties. We also define the size of the absorbing layer as 10 grid points
-    model = Model(vp=v, origin=origin, shape=shape, spacing=spacing,
-                  space_order=2, nbl=10, bcs="damp")
+    model = Model(vp=v,origin=origin,shape=shape,spacing=spacing,space_order=sou,nbl=nbl,bcs="damp")
 
     return model
+#==============================================================================
 
+#==============================================================================
+def generateRandomModel_linhas(teste,vmodelmin,vmodelmax,max_layers):
 
-# Define a physical size
-#shape   = (101, 51)  # Number of grid point (nx, nz)
-#spacing = (20., 20.)  # Grid spacing in m. The domain size is now 2km by 1km
-#origin = (0. , 0.)  # What is the location of the top left corner. This is necessary to define
+    shape      = (teste.nptx,teste.nptz)
+    spacing    = (teste.hx,teste.hz)
+    origin     = (teste.x0,teste.z0)
+    sou        = teste.sou
+    nbl        = teste.nbl
+    v          = np.empty(shape, dtype=np.float32)
 
-# Seeding the RNG
-#np.random.seed(int(time.time()))
-# np.random.seed(0)
+    vel_media    = np.sort(np.random.rand(max_layers+1)*int(vmodelmax-vmodelmin) + vmodelmin)
+    vel_media[0] = vmodelmin;
 
-# Generates the model
-#model = generateRandomModel(shape, spacing, origin)
+    layer_type    = np.rint(np.random.rand(max_layers)).astype(int)
+    layer_type[0] = 0;  
 
-#plot_velocity(model)
+    n_layers = np.floor(np.random.rand() * max_layers + 1).astype(int)
+
+    prof    = np.random.rand(n_layers) * np.rint(shape[1]/n_layers);
+    prof[0] = np.rint(10)
+
+    for k in range(n_layers):
+        
+        prof[k] = prof[k] + np.rint(shape[1]/n_layers)*k;
+    
+    k_vec = np.arange(0,n_layers)
+    i_vec = np.arange(0,shape[0])
+    j_vec = np.arange(0,shape[1])
+
+    eq_layer = np.zeros((n_layers, shape[0]))
+    
+    for k in range(0, n_layers):
+        
+        if(layer_type[k]==0):
+        
+            eq_layer[k, :] = prof[k]; 
+                        
+    i_vec = np.array([i_vec,]*shape[1]).transpose()
+    j_vec = np.array([j_vec,]*shape[0])
+
+    v[:,:] = vel_media[n_layers]
+    
+    for k in range(n_layers-1, -1, -1):
+        
+        indices = j_vec < eq_layer[k, i_vec]
+        
+        v[np.where(indices)] = vel_media[k]
+
+    model = Model(vp=v,origin=origin,shape=shape,spacing=spacing,space_order=sou,nbl=nbl,bcs="damp")
+
+    return model
+#==============================================================================
+
+#==============================================================================
+def generateRandomModel_linha_inclinada(teste,vmodelmin,vmodelmax,max_layers):
+
+    shape      = (teste.nptx,teste.nptz)
+    spacing    = (teste.hx,teste.hz)
+    origin     = (teste.x0,teste.z0)
+    sou        = teste.sou
+    nbl        = teste.nbl
+    v          = np.empty(shape, dtype=np.float32)
+        
+    vel_media    = np.sort(np.random.rand(max_layers+1)*int(vmodelmax-vmodelmin) + vmodelmin)
+    vel_media[0] = vmodelmin;
+
+    layer_type    = np.rint(np.random.rand(max_layers)).astype(int)
+    layer_type[0] = 0;
+
+    n_layers = np.floor(np.random.rand() * max_layers + 1).astype(int)
+
+    prof    = np.random.rand(n_layers) * np.rint(shape[1]/n_layers);
+    prof[0] = np.rint(10)
+
+    for k in range(n_layers):
+        prof[k] = prof[k] + np.rint(shape[1]/n_layers)*k;
+
+    k_vec = np.arange(0,n_layers)
+    i_vec = np.arange(0,shape[0])
+    j_vec = np.arange(0,shape[1])
+
+    eq_layer = np.zeros( (n_layers, shape[0]) )
+
+    incl = np.random.rand(max_layers) * 0.2 - 0.1;
+    
+    for k in range(0,n_layers):
+        
+        if(layer_type[k]==0):
+        
+            eq_layer[k,:] = incl[k] * i_vec + prof[k]; 
+              
+    i_vec = np.array([i_vec,]*shape[1]).transpose()
+    j_vec = np.array([j_vec,]*shape[0])
+
+    v[:,:] = vel_media[n_layers]
+    
+    for k in range(n_layers-1, -1, -1):
+    
+        indices = j_vec < eq_layer[k, i_vec]
+        
+        v[np.where(indices)] = vel_media[k]
+
+    model = Model(vp=v,origin=origin,shape=shape,spacing=spacing,space_order=sou,nbl=nbl,bcs="damp")
+
+    return model
+#==============================================================================
+
+#==============================================================================
+def generateRandomModel_gaussiana(teste,vmodelmin,vmodelmax,max_layers):
+
+    shape      = (teste.nptx,teste.nptz)
+    spacing    = (teste.hx,teste.hz)
+    origin     = (teste.x0,teste.z0)
+    sou        = teste.sou
+    nbl        = teste.nbl
+    v          = np.empty(shape, dtype=np.float32)
+        
+    vel_media    = np.sort(np.random.rand(8) * 2 + vmodelmin)
+    vel_media[0] = vmodelmin;
+
+    layer_type    = np.rint(np.random.rand(max_layers)).astype(int)
+    layer_type[0] = 0;
+
+    n_layers = np.floor(np.random.rand() * max_layers + 1).astype(int)
+
+    prof    = np.random.rand(n_layers) * np.rint(shape[1]/n_layers);
+    prof[0] = np.rint(10)
+
+    for k in range(n_layers):
+        prof[k] = prof[k] + np.rint(shape[1]/n_layers)*k;
+
+    k_vec = np.arange(0,n_layers)
+    i_vec = np.arange(0,shape[0])
+    j_vec = np.arange(0,shape[1])
+
+    eq_layer = np.zeros( (n_layers, shape[0]) )
+
+    ampl = np.random.rand(max_layers) * 20;
+    wlen = np.random.rand(max_layers) * 0.5;
+    cent = np.random.rand(max_layers) * shape[0];
+
+    for k in range(0, n_layers):
+        
+        if(layer_type[k]== 0):
+       
+            eq_layer[k, :] = prof[k]; 
+              
+        else:
+            
+            eq_layer[k,:] = -ampl[k] * np.exp(-0.05 * wlen[k] * (i_vec-cent[k]) * (i_vec-cent[k])) + prof[k]
+            
+    i_vec = np.array([i_vec,]*shape[1]).transpose()
+    j_vec = np.array([j_vec,]*shape[0])
+
+    v[:,:] = vel_media[n_layers]
+    
+    for k in range(n_layers-1, -1, -1):
+    
+        indices = j_vec < eq_layer[k, i_vec]
+        
+        v[np.where(indices)] = vel_media[k]
+
+    model = Model(vp=v,origin=origin,shape=shape,spacing=spacing,space_order=sou,nbl=nbl,bcs="damp")
+
+    return model
+#==============================================================================
--- examples/seismic/cemeai/modelos.py
+++ examples/seismic/cemeai/modelos.py
@@ -3,9 +3,17 @@
 #==============================================================================
 import numpy              as np
 import matplotlib.pyplot  as plot
-import time               as tm
-import time
+import segyio
 import sys
+#==============================================================================
+
+#==============================================================================
+# Rotinas Próprias
+#==============================================================================
+import testes             as tt
+import rotinas_plot       as rplot
+import gerar_modelos      as gr
+import dados_marmousi     as ddmarmou
 #==============================================================================
 
 #==============================================================================
@@ -15,160 +23,255 @@ from examples.seismic import Model, plot_velocity
 from examples.seismic import TimeAxis
 from examples.seismic import RickerSource
 from examples.seismic import Receiver
-from devito import TimeFunction
-from devito import Eq, solve
-from devito import Operator
 from examples.seismic import plot_shotrecord
-from devito import configuration
-from gerar_modelos import generateRandomModel
+from devito           import TimeFunction
+from devito           import Eq, solve
+from devito           import Operator
+from devito           import configuration
+#==============================================================================
+
+#==============================================================================
+plot.close("all")
 configuration['log-level']='ERROR'
 #==============================================================================
 
 #==============================================================================
-plot.close("all") # fecha todas as janelas
+# Importando Dados do Problema
+#==============================================================================
+teste     = tt.teste2
+sintmodel = teste.sintmodel                                   
+#==============================================================================
+
+#==============================================================================
+# Capturando Informações do Modelo Sintético
+#==============================================================================
+if(sintmodel==1):
+
+    C0        = ddmarmou.inter_marmousi(teste)
+    V1        = ddmarmou.graph2dvel(C0,teste)
+    vmodelmin = np.amin(C0)
+    vmodelmax = np.amax(C0)
+
+else:
+    
+    vmodelmin = 1.5
+    vmodelmax = 3.5
 #==============================================================================
 
 #==============================================================================
 # Parâmetros do Problema 
 #==============================================================================
-nmodelos   = 20000
-
-nptx       = 101
-nptz       = 51
-x0         = 0.0
-x1         = 2000
-z0         = 0.0 
-z1         = 1000
-compx      = x1-x0
-compz      = z1-z0
-hx         = (x1-x0)/(nptx-1)           
-hz         = (z1-z0)/(nptz-1)           
-sou        = 2
-tou        = 2
-nbl        = 10 
-t0         = 0.
-tn         = 1000.
-CFL        = 0.4
-nfonte     = 1
-f0         = 0.020 
-nxfontpos  = 20.   
-nzfontpos  = 20.                        
-nrec       = 51
-nxrecpos   = np.linspace(x0,x1,nrec)   
-nzrecpos   = 20.            
-
-
-verbosity = 0
-
+ncamadas    = teste.ncamadas # Número de Tipos de Camadas
+N0          = teste.N0       # Número de Camadas aleatórias com retas horizontais
+N1          = teste.N1       # Número de Camadas aleatórias com camadas retas inclinadas
+N2          = teste.N2       # Número de Camadas aleatórias com camadas Gaussianas
+N3          = teste.N3       # Número de Camadas aleatórias com camadas Mistas
+nptx        = teste.nptx
+nptz        = teste.nptz
+x0          = teste.x0
+x1          = teste.x1
+z0          = teste.z0
+z1          = teste.z1
+compx       = teste.compx
+compz       = teste.compz
+hx          = teste.hx           
+hz          = teste.hz           
+sou         = teste.sou
+tou         = teste.tou
+nbl         = teste.nbl
+t0          = teste.t0
+tn          = teste.tn
+ntmax       = teste.ntmax
+CFL         = teste.CFL
+nfonte      = teste.nfonte
+f0          = teste.f0
+nxfontposv  = teste.nxfontpos
+nzfontpos   = teste.nzfontpos            
+nrec        = teste.nrec
+nxrecpos    = teste.nxrecpos   
+nzrecpos    = teste.nzrecpos 
+verbosity   = teste.verbosity
+max_layers  = teste.max_layers
 #==============================================================================
 
 #==============================================================================
-ini=15000
-for i in range(ini,nmodelos+ini):
+# Variáveis Locais
+#==============================================================================
+number_xfontpos = np.size(nxfontposv)
+shape           = (nptx,nptz)  
+spacing         = (hx,hz)  
+origin          = (x0,z0)  
+#==============================================================================
+
+#==============================================================================
+# Modelo de Velocidade Hohomogeneo
+#==============================================================================
+vhomo      = np.empty(shape,dtype=np.float32)
+vhomo[:,:] = vmodelmin     
+model0     = Model(vp=vhomo,origin=origin,shape=shape,spacing=spacing,space_order=sou,nbl=nbl,bcs="damp")
+rec_homo   = np.zeros((number_xfontpos,ntmax+1,nrec))
+#==============================================================================
+
+#==============================================================================
+# For para o Número de Fontes
+#==============================================================================
+for k in range(0,number_xfontpos):
+    
+    nxfontpos = nxfontposv[k]
+#==============================================================================
+
+#==============================================================================
+# Construção Parâmetros Temporais do Modelo Teste
+#==============================================================================
+    dt_ref0 = model0.critical_dt 
+    dt0     = (tn-t0)/(ntmax)
+    
+    if(dt0>dt_ref0):
+    
+        print("Warning: dt: ", dt0, " dt_ref: ", dt_ref0)
+    
+    time_range0 = TimeAxis(start=t0,stop=tn,step=dt0)
+#==============================================================================
+
+#==============================================================================
+# Construção Fonte de Ricker Modelo Homogeneo
+#==============================================================================
+    src0 = RickerSource(name='src0',grid=model0.grid,f0=f0,npoint=nfonte,time_range=time_range0)
+    src0.coordinates.data[:, 0] = nxfontpos
+    src0.coordinates.data[:, 1] = nzfontpos
+#==============================================================================
+
+#==============================================================================
+# Construção Receivers Homogeneo
+#==============================================================================
+    rec0 = Receiver(name='rec0', grid=model0.grid,npoint=nrec,time_range=time_range0)
+    rec0.coordinates.data[:,0] = nxrecpos
+    rec0.coordinates.data[:,1] = nzrecpos 
+#==============================================================================
+
+#==============================================================================
+# Construção dos Campos Modelo Homogêneo
+#==============================================================================
+    u0         = TimeFunction(name="u0",grid=model0.grid,time_order=tou,space_order=sou)
+    pde0       = model0.m * u0.dt2 - u0.laplace + model0.damp * u0.dt
+    stencil0   = Eq(u0.forward, solve(pde0, u0.forward))
+    src_term0  = src0.inject(field=u0.forward, expr=src0* dt0**2 / model0.m)
+    rec_term0  = rec0.interpolate(expr=u0.forward)
+#==============================================================================
+
+#==============================================================================
+# Construção e Execução dos Operadores Modelo Homogeneo e Modelo Teste
+#==============================================================================
+    op0 = Operator([stencil0] + src_term0 + rec_term0, subs=model0.spacing_map)
+    op0(time=time_range0.num-1,dt=model0.critical_dt)
+#==============================================================================
+
+#==============================================================================
+# Salvando Informações dos Receiveris do Modelo Homogeneo
+#==============================================================================
+    rec_homo[k,:,:] = rec0.data[:,:]
+#==============================================================================
+
+#==============================================================================
+# Variando os Tipos de Camadas Aleatórias
+#==============================================================================
+for tmodel in range(0,ncamadas):
+    
+    if(tmodel==0): nmodelos = N0
+    if(tmodel==1): nmodelos = N1
+    if(tmodel==2): nmodelos = N2
+    if(tmodel==3): nmodelos = N3
 #==============================================================================
     
 #==============================================================================
-# Parâmetros de Malha
+# For para o Número de Modelos
 #==============================================================================
-    shape   = (nptx,nptz)  
-    spacing = (hx,hz)  
-    origin  = (x0,z0)  
+    for i in range(0,nmodelos):
 #==============================================================================
-
-#==============================================================================
-# Parâmetros de Velocidade
-#==============================================================================
-    v = np.empty(shape,dtype=np.float32)
-    v[:, :51] = 1.5
-    v[:, 51:] = 1.5
-#==============================================================================
-
-
+    
 #==============================================================================
 # Construção do Modelo de Velocidade
+#==============================================================================     
+        if(tmodel==0): model = gr.generateRandomModel_linhas(teste,vmodelmin,vmodelmax,max_layers)
+        if(tmodel==1): model = gr.generateRandomModel_linha_inclinada(teste,vmodelmin,vmodelmax,max_layers)
+        if(tmodel==2): model = gr.generateRandomModel_gaussiana(teste,vmodelmin,vmodelmax,max_layers)
+
+        if(verbosity>0):
+            
+            rplot.graph2dvel(model.vp.data,teste,i,tmodel)    
 #==============================================================================
-    plt_name="data_save/model_plt_%d"%i 
-    print(i)
 
-    if i>0:
-        # Seeding the RNG
-        np.random.seed(int(time.time()))
+#==============================================================================
+# Salvando Informacoes de Velocidade
+#==============================================================================
+        field = np.transpose(model.vp.data[nbl:-nbl,nbl:-nbl])
+        np.save("data_save/vel_model_data_%d_type_%d"%(i,tmodel),field)
+#==============================================================================
 
-        # Generates the model
-        #start = time.time()
-        model = generateRandomModel(shape, spacing, origin)
-        #end = time.time()
-        #print(end - start)
-        if verbosity > 0:
-            plot_velocity(model, pltname=plt_name)
+#==============================================================================
+# Construção Parâmetros Temporais do Modelo Teste
+#==============================================================================
+        dt_ref = model.critical_dt 
+        dt     = (tn-t0)/(ntmax)
     
-    else: # homogeneous condition, reference
-        model = Model(vp=v,origin=origin,shape=shape,spacing=spacing,space_order=sou,nbl=nbl,bcs="damp")
-        if verbosity > 0:
-            plot_velocity(model, pltname=plt_name)
-
-#==============================================================================
-    field = np.transpose(model.vp.data)
-
-    #print(field)
-    np.save("data_save/model_data_%d"%i,field)
-
-#==============================================================================
-# Construção Parâmetros Temporais
-#==============================================================================
-    dt_ref         = model.critical_dt 
-    #time_range = TimeAxis(start=t0,stop=tn,step=dt)
+        if(dt>dt_ref):
     
-    #vmax  = np.amax(v) 
-    #dtmax = (min(hx,hz)*CFL)/(vmax)   
-    #ntmax = int((tn-t0)/dtmax)+1
-    ntmax = 401
-    dt    = (tn-t0)/(ntmax)
-    if dt > dt_ref:
-        print("Warning: dt:", dt, " dt_ref:", dt_ref)
-    time_range = TimeAxis(start=t0,stop=tn,step=dt)
+            print("Warning: dt: ", dt, " dt_ref: ", dt_ref)
+    
+        time_range = TimeAxis(start=t0,stop=tn,step=dt)
 #==============================================================================
 
 #==============================================================================
-# Construção Fonte de Ricker
+# For para o Número de Fontes
 #==============================================================================
-    src = RickerSource(name='src', grid=model.grid, f0=f0,npoint=nfonte, time_range=time_range)
-    src.coordinates.data[:, 0] = nxfontpos
-    src.coordinates.data[:, 1] = nzfontpos
-#==============================================================================
-
-#==============================================================================
-# Construção Receivers
-#==============================================================================
-    rec = Receiver(name='rec', grid=model.grid, npoint=nrec, time_range=time_range)
-    rec.coordinates.data[:, 0] = nxrecpos
-    rec.coordinates.data[:, 1] = nzrecpos 
+        for k in range(0,number_xfontpos):
+    
+            nxfontpos = nxfontposv[k]
 #==============================================================================
 
 #==============================================================================
-# Construção dos Campos
+# Construção Fonte de Ricker Modelo Teste
 #==============================================================================
-    u        = TimeFunction(name="u",grid=model.grid, time_order=tou, space_order=sou)
-    pde      = model.m * u.dt2 - u.laplace + model.damp * u.dt
-    stencil  = Eq(u.forward, solve(pde, u.forward))
-    src_term = src.inject(field=u.forward, expr=src * dt**2 / model.m)
-    rec_term = rec.interpolate(expr=u.forward)
+            src = RickerSource(name='src', grid=model.grid,f0=f0,npoint=nfonte,time_range=time_range)
+            src.coordinates.data[:, 0] = nxfontpos
+            src.coordinates.data[:, 1] = nzfontpos
 #==============================================================================
 
 #==============================================================================
-# Construção dos Operadores
+# Construção Receivers Modelo Teste
 #==============================================================================
-    op = Operator([stencil] + src_term + rec_term, subs=model.spacing_map)
-    start = tm.time()
-    op(time=time_range.num-1, dt=model.critical_dt)
-    end   = tm.time() 
-    #print("Elapsed (after compilation) = %s" % (end - start))
+            rec = Receiver(name='rec',grid=model.grid,npoint=nrec,time_range=time_range)
+            rec.coordinates.data[:,0] = nxrecpos
+            rec.coordinates.data[:,1] = nzrecpos 
 #==============================================================================
 
 #==============================================================================
-# Manipulando Receivers
+# Construção dos Campos Modelo Teste
 #==============================================================================
-    np.save("data_save/rec_data_%d"%i,rec.data)
-    if verbosity > 0:
-        plot_shotrecord(rec.data, model, t0, tn)
+            u        = TimeFunction(name="u",grid=model.grid, time_order=tou, space_order=sou)
+            pde      = model.m * u.dt2 - u.laplace + model.damp * u.dt
+            stencil  = Eq(u.forward, solve(pde, u.forward))
+            src_term = src.inject(field=u.forward, expr=src * dt**2 / model.m)
+            rec_term = rec.interpolate(expr=u.forward)
+#==============================================================================
+
+#==============================================================================
+# Construção e Execução dos Operadores Modelo Homogeneo e Modelo Teste
+#==============================================================================
+            op  = Operator([stencil] + src_term + rec_term, subs=model.spacing_map)        
+            op(time=time_range.num-1,dt=model.critical_dt)
+#==============================================================================
+
+#==============================================================================
+# Manipulando Receivers com Corte da Onda Direta
+#==============================================================================
+            rec_clean = np.zeros((rec.data.shape[0],rec.data.shape[1]))
+            rec_clean = rec.data - rec_homo[k,:,:]
+                        
+            np.save("data_save/rec_data_model_%d_source_%d_type_%d"%(i,k,tmodel),rec_clean[:,nbl:-nbl])
+    
+            if(verbosity>0):
+    
+                rplot.graph2d(u.data[0,:,:],teste,i,k,tmodel)
+                rplot.graph2drec(rec_clean,teste,i,k,tmodel)
 #==============================================================================
